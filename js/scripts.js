a9389a1c401eae6ce4c5adc3b2488f83bafa8450
jacob ruleaux and max scher <max@>
2017-05-31 13:57:28 -0700

    finalize changes to js buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   $("button#cat-talk").click(function() {
     $("h4#cat-says").after("<p>meow!</p>");
     $("h4#dog-says").after("<p>bark!</p>");
+    $("p").click(function() {
+      $(this).remove();
+    });
   });
   $("button#dog-talk").click(function() {
     $("h4#cat-says").after("<p>meow!</p>");
     $("h4#dog-says").after("<p>bark!</p>");
   });
+  
 });
